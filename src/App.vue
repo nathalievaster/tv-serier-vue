931fa8ebb35a2caa1ecf4170c29803ddc0b044d2
Nathalie Fredriksson <nfredriksson1@gmail.com>
2025-12-15 19:21:31 +0100

    enkel navigering

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,16 @@ import { RouterView, RouterLink } from 'vue-router'
 
 <template>
   <RouterView />
+  <header>
+    <h1>TV Serier</h1>
+  <nav>
+    <ul>
+      <li><RouterLink to="/">Home</RouterLink></li>
+      <li><RouterLink to="/list">List</RouterLink></li>
+      <li><RouterLink to="/info">Info</RouterLink></li>
+    </ul>
+  </nav>
+  </header>
 </template>
 
 <style scoped></style>
---
+++ src/views/InfoView.vue
@@ -0,0 +1,12 @@
+<script setup>
+</script>
+
+<template>
+  <article class="home">
+    <h2>Welcome to TV Serier</h2>
+    <p>Your go-to app for tracking and discovering TV series.</p>
+    </article>
+</template>
+
+<style scoped>
+</style>
