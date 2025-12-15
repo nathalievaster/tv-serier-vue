0dce7528c3e52f819b8fcbc5679f212e8c7828bb
Nathalie Fredriksson <nfredriksson1@gmail.com>
2025-12-15 21:22:38 +0100

    grund för att kunna läsa ut objekt från serie-api

---
+++ src/components/SeriesItem.vue
@@ -0,0 +1,17 @@
+<template>
+    <article> {{ serie.serie }} </article>
+</template>
+
+<script setup>
+    const props = defineProps({
+        serie: Object
+    });
+</script>
+
+<style scoped>
+    p {
+        background-color: black;
+        padding: 10px;
+        color: white;
+    }
+</style>
--- src/views/ListView.vue
+++ src/views/ListView.vue
@@ -1,6 +1,27 @@
 <script setup>
 import Header from "../components/Header.vue";
 import Footer from "../components/Footer.vue";
+import SeriesItem from "../components/SeriesItem.vue";
+import { ref } from "vue";
+
+const series = ref([
+ {
+  serie: "Stranger Things",
+  _id: 1
+ },
+  {
+    serie: "The Crown",
+    _id: 2
+  },
+  {
+    serie: "Breaking Bad",
+    _id: 3
+  },
+  {
+    serie: "The Mandalorian",
+    _id: 4
+  }
+]);
 </script>
 
 <template>
@@ -10,6 +31,7 @@ import Footer from "../components/Footer.vue";
       <h2>Lägg till tv-serier i listan</h2>
       <p>Lägg till eller ta bort tv-serier ur listan nedan.</p>
     </article>
+    <SeriesItem v-for="serie in series" :serie="serie" :key="serie._id"/>
   </main>
   <Footer />
 </template>
