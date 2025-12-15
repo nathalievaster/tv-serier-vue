3570f5f52577bf7b84bd0e1f797764a9d4a63da9
Nathalie Fredriksson <nfredriksson1@gmail.com>
2025-12-15 19:15:00 +0100

    grundstruktur för rutter och vyer

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,27 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+import ListView from '../views/ListView.vue'
+import InfoView from '../views/InfoView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: HomeView
+    },
+    {
+      path: '/list',
+      name: 'list',
+      component: ListView
+    },
+    {
+      path: '/info',
+      name: 'info',
+      component: InfoView
+    }
+  ],
 })
 
 export default router
