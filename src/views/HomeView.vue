4cd0e5535d98fb7e09156624e1f99068b7cb4013
Nathalie Fredriksson <nfredriksson1@gmail.com>
2025-12-15 20:41:11 +0100

    banner till startsidan

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,17 +1,30 @@
 <script setup>
 import Header from "../components/Header.vue";
 import Footer from "../components/Footer.vue";
+import bannerImage from '../assets/banner.webp'
 </script>
 
 <template>
   <Header />
+
+  <!-- Banner -->
+<section class="relative w-full h-64 md:h-96 mb-6">
+  <img :src="bannerImage" alt="Bannerbild" class="w-full h-full object-cover block" />
+  <!--- Overlay -->
+    <div class="absolute inset-0 bg-black bg-opacity-50 flex items-center justify-center">
+      <h2 class="text-white text-3xl md:text-5xl font-bold">TV-serier</h2>
+    </div>
+  </section>
+
   <main class="p-6 max-w-4xl mx-auto">
     <article class="home">
-      <h2 class="font-bold">Välkommen till tv-serier!</h2>
-      <p>Bla bla bla.</p>
+      <h2 class="font-bold text-2xl mb-2">Välkommen till tv-serier!</h2>
+      <p>Här kan du skriva kort information om ditt fritidsintresse.</p>
     </article>
   </main>
+
   <Footer />
 </template>
+
 
 <style scoped></style>
