a562c1e82efd6163351d042afd10311098240361
Nathalie Fredriksson <nfredriksson1@gmail.com>
2025-12-15 20:08:54 +0100

    inkluderar tailwind och använder för header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -3,15 +3,36 @@ import { RouterView, RouterLink } from 'vue-router'
 </script>
 
 <template>
-    <RouterView />
-  <header>
-    <h1>TV Serier</h1>
-  <nav>
-    <ul>
-      <li><RouterLink to="/">Home</RouterLink></li>
-      <li><RouterLink to="/list">List</RouterLink></li>
-      <li><RouterLink to="/info">Info</RouterLink></li>
-    </ul>
-  </nav>
+
+    <header class="bg-white shadow p-4">
+      <h1 class="text-3xl font-bold text-center mb-3">TV Serier</h1>
+
+      <nav>
+        <ul class="flex justify-center gap-6 text-lg">
+          <li>
+            <RouterLink
+              to="/"
+              class="hover:text-blue-600 transition-colors font-medium"
+            >Startsida</RouterLink>
+          </li>
+          <li>
+            <RouterLink
+              to="/list"
+              class="hover:text-blue-600 transition-colors font-medium"
+            >Tv-serier</RouterLink>
+          </li>
+          <li>
+            <RouterLink
+              to="/info"
+              class="hover:text-blue-600 transition-colors font-medium"
+            >Info</RouterLink>
+          </li>
+        </ul>
+      </nav>
     </header>
+
+    <main class="p-6 max-w-4xl mx-auto">
+      <RouterView />
+    </main>
+
 </template>
--- src/views/InfoView.vue
+++ src/views/InfoView.vue
@@ -1,4 +1,5 @@
 <script setup>
+  import Header from '../components/Header.vue'
 </script>
 
 <template>
